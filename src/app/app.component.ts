868f44da7a7de1b22077e822bbb0bf2f14a3079c
gowtham-9 <puppalagowthamsai@gmail.com>
2021-01-02 19:16:47 -0500

    Title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularApp1';
+  title = 'AngularApp1 is a sample component';
 }
